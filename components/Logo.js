ec953bee930f4bfaa476281740fb72236335dac0
manoj-nathwani <manoj_1993@hotmail.com>
2021-11-11 11:17:59 +0000

    updated logo

--- components/Logo.js
+++ components/Logo.js
@@ -5,7 +5,7 @@ export default function Logo() {
     return (
         <span className="text-danger">
             <FontAwesomeIcon icon={faCompass} className="me-2" />
-            <span>UNAIDS Tools Navigator</span>
+            <span>HIV Estimates Navigator</span>
         </span>
     )
 }
