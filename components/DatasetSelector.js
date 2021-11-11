f267e97a6924310356dcd79380c4243a2d0475d9
manoj-nathwani <manoj_1993@hotmail.com>
2021-11-11 17:58:35 +0000

    WIP - fixed dataset fetching in DatasetSelector

--- components/DatasetSelector.js
+++ components/DatasetSelector.js
@@ -4,17 +4,30 @@ import {
 } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { makeUseAxios } from 'axios-hooks'
+import { baseAxiosConfig, getDatasets } from '../lib/api';
+
+const useAxios = makeUseAxios(baseAxiosConfig)
 
 export default function DatasetSelector(props) {
     const [showModal, setShowModal] = useState(false);
 
+    const [
+        {
+            data: datasets,
+            loading: datasetsLoading,
+        },
+        fetchDatasets
+    ] = useAxios(getDatasets, { manual: true });
+
     function SelectDatasetInput() {
         const addNewDatasetOption = {
-            id: 0,
+            id: "0",
             name: '+ Add New Dataset'
         };
         const handleChange = e => {
             if (e.target.value === addNewDatasetOption.id) {
+                fetchDatasets();
                 setShowModal(true);
             } else {
                 props.setCurrentDatasetId(e.target.value);
@@ -38,6 +51,21 @@ export default function DatasetSelector(props) {
         )
     }
 
+    const listView = () => (
+        <ListGroup variant="flush">
+            {datasets && datasets.map(dataset =>
+                <ListGroup.Item
+                    key={dataset.id}
+                    action
+                    onClick={() => alert(dataset.id)}
+                >
+                    <FontAwesomeIcon icon={faFolder} className="me-2" />
+                    <span>{dataset.name}</span>
+                </ListGroup.Item>
+            )}
+        </ListGroup>
+    )
+
     function AddNewDatasetModal() {
         return (
             <Modal show={showModal} onHide={() => setShowModal(false)}>
@@ -45,23 +73,7 @@ export default function DatasetSelector(props) {
                     <Modal.Title>Add New Dataset</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <Row>
-                                <Col xs={2}><Image src="https://adr.unaids.org/uploads/group/2019-08-30-082314.463879Ug-logo.jpg" fluid /></Col>
-                                <Col>
-                                    <h4>Uganda</h4>
-                                    <a href="https://adr.unaids.org/organization/uganda" target="_blank" rel="noreferrer" className="link-danger">adr.unaids.org/organization/uganda</a>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        {props.user.datasets.map(dataset =>
-                            <ListGroup.Item key={dataset.id} action>
-                                <FontAwesomeIcon icon={faFolder} className="me-2" />
-                                <span>{dataset.name}</span>
-                            </ListGroup.Item>
-                        )}
-                    </ListGroup>
+                    {datasetsLoading ? 'Loading...' : listView()}
                 </Modal.Body>
             </Modal>
         )
