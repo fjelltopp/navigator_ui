fc3d9011663ea77bc0577f06f258f358ff710eba
manoj-nathwani <manoj_1993@hotmail.com>
2021-12-03 13:47:33 +0000

    removed nextjs Head due to favicon bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import AuthWrapper from '../components/AuthWrapper';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,10 +10,8 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
-        <title>HIV Estimates Navigator</title>
-      </Head>
+      <link rel="icon" href="/images/favicon.ico" />
+      <title>HIV Estimates Navigator</title>
       {insecurePages.includes(asPath)
         ? <Component {...pageProps} />
         : <AuthWrapper {...{ Component, pageProps }} />
