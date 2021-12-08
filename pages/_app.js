5465f3cc80f0c70f1f9ead330d343aa60b789729
Tomasz Sabała <tomek@fjelltopp.org>
2021-12-08 11:07:41 +0100

    NAV-213 Google analytics for next js navigator ui.

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import { useRouter } from 'next/router';
 import AuthWrapper from '../components/AuthWrapper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import Script from 'next/script'
 import Head from 'next/head'
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
 export default function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter();
@@ -12,8 +15,27 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-          <title>HIV Estimates Navigator</title>
+        <title>HIV Estimates Navigator</title>
       </Head>
+      {GA_ID.length > 0 &&
+        <Script
+          strategy='lazyOnload'
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+      }
+      {GA_ID.length > 0 &&
+        <Script id='ga-analytics'>
+          {
+            `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_ID}');
+            `
+          }
+        </Script>
+      }
       {insecurePages.includes(asPath)
         ? <Component {...pageProps} />
         : <AuthWrapper {...{ Component, pageProps }} />
