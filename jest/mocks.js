83e3c2b83a0df7f892461f3d672e71016f36ce74
manoj-nathwani <manoj_1993@hotmail.com>
2022-01-17 16:56:09 +0000

    NAV-115-testing-framework

---
+++ jest/mocks.js
@@ -0,0 +1,15 @@
+export const mockedApiResponses = {
+    "/user": {
+        "email": "test@test.org",
+        "fullname": "Mr Test Man"
+    },
+    "/datasets": {
+        "datasets": [
+            {
+                "id": "test-test-test-test-test",
+                "name": "Test HIV Estimates 2022",
+                "organizationName": "Test Org"
+            }
+        ]
+    }
+}
