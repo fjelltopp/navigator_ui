22b9d387e433151705428e96e2d7ffb55048e712
manoj-nathwani <manoj_1993@hotmail.com>
2022-01-24 09:59:02 +0000

    cleanup

--- pages/login.js
+++ pages/login.js
@@ -58,7 +58,6 @@ export default function Login({ }) {
             <h3 className="text-center"><Logo /></h3>
             <hr />
             <p>{t('welcome')}</p>
-            <p>Welcome to the UNAIDS HIV Estimates Navigator. Your HIV estimates journey begins here!</p>
             <p>The HIV Estimates Navigator (“Navigator”) is the latest tool provided by UNAIDS to assist country teams to produce their annual HIV estimates. The Navigator is an automated, step-by-step assistant for estimates teams. Whether you have participated in the estimates for many years or it’s your first time, the Navigator will guide you through the process across all estimates tools and models. From generating your input data to selecting advanced options and fitting your models, Navigator provides detailed, step-by-step instructions and resources to assist you along the way. Need to step away for a bit? No problem, Navigator will help you pick up where you left off, telling you what's next and what tasks remain to be done.</p>
             <hr />
             <ErrorBanner />
