6eac02fd6b2036ad7b22a6b880a73141e14d335e
Chas Nelson <github@chasnelson.co.uk>
2022-10-20 12:23:38 +0100

    fix: remove sandbox secrets

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,8 @@ FROM node:lts-buster-slim as builder
 WORKDIR /app
 ARG NEXT_PUBLIC_CKAN_SITE_URL=https://adr.unaids.org
 ARG NEXT_PUBLIC_API_BASE_URL=http://navigator.fjelltopp.org/middleware/api
-ARG AUTH0_SECRET='3dea95744927fd8b1a6e7a60cac85e06341d57da23a94bd2fef45493c4dcbdae'
 ARG AUTH0_BASE_URL='http://navigator.minikube'
 ARG AUTH0_ISSUER_BASE_URL='https://hivtools.eu.auth0.com'
-ARG AUTH0_CLIENT_ID='BtxZABY9WVbR8E3GWnR5nN7KyTVQHab9'
-ARG AUTH0_CLIENT_SECRET='C15B9gKdIsnbqY8xwTilVyeAwry2otcvehauQxkTMaTwB4lZeUNRkdx4CgKX_5CN'
 ARG AUTH0_AUDIENCE='http://navigator.minikube'
 ARG ARG NODE_ENV=development
 ARG NEXT_TELEMETRY_DISABLED=1
--- pages/login.js
+++ pages/login.js
@@ -8,136 +8,85 @@ import Logo from "@/components/Logo";
 import LocaleSelector from "@/components/LocaleSelector";
 
 const logos = [
-  "/images/unaids.png",
-  "/images/imperial_college.png",
-  "/images/fjelltopp.png",
-  "/images/avenir_health.png",
-  "/images/strategy4ward.png",
+    "/images/unaids.png",
+    "/images/imperial_college.png",
+    "/images/fjelltopp.png",
+    "/images/avenir_health.png",
+    "/images/strategy4ward.png",
 ];
 
 export default function Login() {
-  const router = useRouter();
-  const { locale } = router;
-  const { redirectPath } = router.query;
-  // const useAxios = makeUseAxios(baseAxiosConfig(locale));
+    const router = useRouter();
+    const { locale } = router;
+    const { redirectPath } = router.query;
 
-  // const [
-  //     {
-  //         data: loginState,
-  //         loading: loginStateLoading,
-  //         error: loginStateError
-  //     },
-  //     loginRequest
-  // ] = useAxios(loginApiRequest, { manual: true });
+    function LinkToCkanSite() {
+        return (
+            <a
+                href={process.env.NEXT_PUBLIC_CKAN_SITE_URL}
+                target="_blank"
+                rel="noreferrer"
+                className="link-danger"
+            >
+                {process.env.NEXT_PUBLIC_CKAN_SITE_URL}
+            </a>
+        );
+    }
 
-  // function ErrorBanner() {
-  //     return (
-  //         loginStateError &&
-  //         <Alert variant={'danger'} className="mt-2">
-  //             {(
-  //                 loginStateError.message.includes('401')
-  //                     ? t`Login failed. Bad username or password.`
-  //                     : loginStateError.message
-  //             )}
-  //         </Alert>
-  //     )
-  // }
-
-  // if (loginState) {
-  //     router.push(redirectPath || '/', undefined, { locale });
-  //     return null;
-  // }
-  // const handleSubmit = event => {
-  //     event.preventDefault();
-  //     const username = event.currentTarget.elements.username.value;
-  //     const password = event.currentTarget.elements.password.value;
-  //     loginRequest({ data: { username, password } })
-  //         .catch(error => console.error(error))
-  // }
-  function LinkToCkanSite() {
     return (
-      <a
-        href={process.env.NEXT_PUBLIC_CKAN_SITE_URL}
-        target="_blank"
-        rel="noreferrer"
-        className="link-danger"
-      >
-        {process.env.NEXT_PUBLIC_CKAN_SITE_URL}
-      </a>
+        <LogInLayout>
+            <div className="text-end">
+                <LocaleSelector drop="down" />
+            </div>
+            <h3 className="text-center mb-4">
+                <Logo />
+            </h3>
+            <hr />
+            <p>{t`Welcome to the UNAIDS HIV Estimates Navigator. Your HIV estimates journey begins here!`}</p>
+            <p>{t`The HIV Estimates Navigator ("Navigator") is the latest tool provided by UNAIDS to assist country teams to produce their annual HIV estimates. The Navigator is an automated, step-by-step assistant for estimates teams. Whether you have participated in the estimates for many years or it’s your first time, the Navigator will guide you through the process across all estimates tools and models. From generating your input data to selecting advanced options and fitting your models, Navigator provides detailed, step-by-step instructions and resources to assist you along the way. Need to step away for a bit? No problem, Navigator will help you pick up where you left off, telling you what's next and what tasks remain to be done.`}</p>
+            <hr />
+            <form id="LoginForm" onSubmit={() => { }}>
+                <div>
+                    <p>
+                        <Trans
+                            id="Please login using your <0>LinkToCkanSite</0> login details:"
+                            components={[<LinkToCkanSite />]}
+                        />
+                    </p>
+                    <div>
+                        <Button
+                            as={"a"}
+                            variant="danger"
+                            href={`/api/auth/login`}
+                            className="login-button"
+                        >{t`Sign in with Auth0 SSO`}</Button>
+                    </div>
+                    <div>
+                        <Button
+                            as={"a"}
+                            variant="link"
+                            href={`${process.env.NEXT_PUBLIC_CKAN_SITE_URL}/user/register`}
+                            target="_blank"
+                            className="text-secondary"
+                        >{t`Register`}</Button>
+                        <Button
+                            as={"a"}
+                            variant="link"
+                            href={`${process.env.NEXT_PUBLIC_CKAN_SITE_URL}/user/reset`}
+                            target="_blank"
+                            className="text-secondary float-end"
+                        >{t`Forgot password?`}</Button>
+                    </div>
+                </div>
+            </form>
+            <hr />
+            <Row className="text-center">
+                {logos.map((src, index) => (
+                    <Col key={index}>
+                        <Image src={src} fluid />
+                    </Col>
+                ))}
+            </Row>
+        </LogInLayout>
     );
-  }
-
-  return (
-    <LogInLayout>
-      <div className="text-end">
-        <LocaleSelector drop="down" />
-      </div>
-      <h3 className="text-center mb-4">
-        <Logo />
-      </h3>
-      <hr />
-      <p>{t`Welcome to the UNAIDS HIV Estimates Navigator. Your HIV estimates journey begins here!`}</p>
-      <p>{t`The HIV Estimates Navigator ("Navigator") is the latest tool provided by UNAIDS to assist country teams to produce their annual HIV estimates. The Navigator is an automated, step-by-step assistant for estimates teams. Whether you have participated in the estimates for many years or it’s your first time, the Navigator will guide you through the process across all estimates tools and models. From generating your input data to selecting advanced options and fitting your models, Navigator provides detailed, step-by-step instructions and resources to assist you along the way. Need to step away for a bit? No problem, Navigator will help you pick up where you left off, telling you what's next and what tasks remain to be done.`}</p>
-      <hr />
-      {/* <ErrorBanner /> */}
-      <form id="LoginForm" onSubmit={() => {}}>
-        <div>
-          <p>
-            <Trans
-              id="Please login using your <0>LinkToCkanSite</0> login details:"
-              components={[<LinkToCkanSite />]}
-            />
-          </p>
-          {/* <Form.Group className="mb-3">
-                    <Form.Control
-                        name="username"
-                        type="text"
-                        placeholder={t`Username or Email`}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Control
-                        name="password"
-                        type="password"
-                        placeholder={t`Password`}
-                        required
-                    />
-                </Form.Group>
-                <Button
-                    variant="danger"
-                    disabled={loginStateLoading}
-                    type="submit"
-                >{loginStateLoading ? t`Logging in...` : t`Login`}</Button> */}
-          <Button
-            as={"a"}
-            variant="danger"
-            href={`/api/auth/login`}
-          >{t`Sign in with Auth0 SSO`}</Button>
-          <Button
-            as={"a"}
-            variant="link"
-            href={`${process.env.NEXT_PUBLIC_CKAN_SITE_URL}/user/register`}
-            target="_blank"
-            className="text-secondary"
-          >{t`Register`}</Button>
-          <Button
-            as={"a"}
-            variant="link"
-            href={`${process.env.NEXT_PUBLIC_CKAN_SITE_URL}/user/reset`}
-            target="_blank"
-            className="text-secondary float-end"
-          >{t`Forgot password?`}</Button>
-        </div>
-      </form>
-      <hr />
-      <Row className="text-center">
-        {logos.map((src, index) => (
-          <Col key={index}>
-            <Image src={src} fluid />
-          </Col>
-        ))}
-      </Row>
-    </LogInLayout>
-  );
 }
